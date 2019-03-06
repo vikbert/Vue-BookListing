401fafadd1a90ac83946b4d8f87d0f7edd0e4f0c
Xun Zhou <xun.zhou@lidl.com>
2019-03-06 14:48:18 +0100

    add list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+    </ul>
   </div>
 </template>
 <script>
@@ -8,7 +11,12 @@ export default {
   name: "BookList",
   data() {
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        { title: "Firefox", author: "Firefox Fundation" },
+        { title: "Chrome", author: "Google Fundation" },
+        { title: "Internet Explorer", author: "Microsoft Fundation" }
+      ]
     };
   }
 };
@@ -17,5 +25,13 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
