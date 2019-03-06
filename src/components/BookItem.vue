31442fa58e79060115c90c8149976d5aea2fb541
Xun Zhou <xun.zhou@lidl.com>
2019-03-06 14:52:38 +0100

    add new BookItem

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <li>{{book.title}}: {{book.author}}</li>
+  </div>
+</template>
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"]
+};
+</script>
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
