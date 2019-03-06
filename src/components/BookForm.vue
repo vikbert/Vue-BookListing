896bafd5ff62c563d5e501ed401e4c99eeadb7bd
Xun Zhou <xun.zhou@lidl.com>
2019-03-06 15:38:30 +0100

    create book form

---
+++ src/components/BookForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class>
+    <input v-model="bookTitle" type="text" name="title" value placeholder="book title">
+    <input v-model="bookAuthor" type="text" name="author" value placeholder="book author">
+    <button type="submit" name="button">Add Book</button>
+  </form>
+</template>
+<script>
+export default {
+  name: "BookForm",
+  props: ["books"],
+  data() {
+    return {
+      bookTitle: "",
+      bookAuthor: ""
+    };
+  },
+  methods: {
+    bookSubmit(bookTitle, bookAuthor) {
+      this.$emit("addBook", bookTitle, bookAuthor);
+    }
+  }
+};
+</script>
+<style>
+</style>
